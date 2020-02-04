2be72f8719083373d4b999d7558f730ef2cecdaa
ToffeeDev <sakanrin.top21@gmail.com>
2020-02-04 11:27:45 +0700

    add RootController

---
+++ src/main/java/com/top/demoweb/dto/UserCredentialsDto.java
@@ -0,0 +1,15 @@
+package com.top.demoweb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCredentialsDto {
+  private String username;
+  private String password;
+}
